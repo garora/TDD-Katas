0b5807bda84bcb2e417ab01627808b32a0e29a1d
Gaurav Arora <gaurav.arora6@gmail.com>
2011-10-29 23:45:13 +0530

    Adding Project Folder

---
+++ TDD-Katas-project/TDD-Katas-project/StringCalculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Refere : http://osherove.com/tdd-kata-1/ of String Calculator exercise - detail is in StringCalculator.txt
+namespace TDD_Katas_project.StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            return string.IsNullOrEmpty(numbers) ? 0 : GetSum(numbers);
+        }
+
+        private static int GetSum(string numbers)
+        {
+            var number = numbers.Split(','); //in first step we have only commas [,] as delimiter
+            var sum = 0;
+            if (number.Length > 0)
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    VelidateNumbersArePerRule(Convert.ToInt32(string.IsNullOrEmpty(number[i]) ? "0" : number[i]));
+                }
+
+                sum = number.Sum(n => ParseToInt(n));
+            }
+            return sum;
+        }
+
+        private static int ParseToInt(string n)
+        {
+            var result = Convert.ToInt32(string.IsNullOrEmpty(n) ? "0" : n);
+
+            VelidateNumbersArePerRule(result);
+
+            return result;
+        }
+
+        private static void VelidateNumbersArePerRule(int number)
+        {
+            if (number < 0 || number > 3)
+                throw new ArgumentException(string.Format("string contains [{0}], which does not meet rule. enter numbers between [0-3]", number));
+        }
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/StringCalculator/TestCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace TDD_Katas_project.StringCalculator
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        [Test]
+        public void Add_ReturnZero_WhenSupplied_EmptyOrNullString(string numbers)
+        {
+            var result = Calculator.Add(numbers);
+            Assert.That(result, Is.EqualTo(0));
+        }
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [Test]
+        public void Add_ReturnNumber_WhenSuppliedSingleNumber_InString(string number, int expectedResult)
+        {
+            var result = Calculator.Add(number);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("0,1",1)]
+        [TestCase("0,1,1", 2)]
+        [TestCase("0,2", 2)]
+        [TestCase("0,2,2", 4)]
+        [TestCase("0,3", 3)]
+        [TestCase("0,3,2", 5)]
+        [TestCase("0,3,3", 6)]
+        [TestCase("0,3,2,", 5)]
+        [Test]
+        public void Add_ReturnSum_WhenSuppliedMultipleNumbers_InString(string numbers, int expectedResult)
+        {
+            var result = Calculator.Add(numbers);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
