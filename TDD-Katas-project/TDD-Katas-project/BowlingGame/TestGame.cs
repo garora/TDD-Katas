1b5f7323e38e0be566f1cc5ca02214d460c54119
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-16 18:49:23 +0530

    Refactoring

--- TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
@@ -4,25 +4,39 @@ using NUnit.Framework;
 namespace TDD_Katas_project.BowlingGame
 {
     [TestFixture]
+    [Category("TheBowlingGameKata")]
     public class TestGame
     {
-        private Game _game = null;
-        [SetUp]
+        private Game _game;
+
+        #region Setup/TearDown
+        [TestFixtureSetUp]
         public void Setup()
         {
             _game = new Game();
         }
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            _game = null;
+        }
+        #endregion
+
+        #region TestMethods
+
         [Test]
         [TestCase(0, 0)]
         [TestCase(1, 20)]
-        [TestCase(5,18)]
+        [TestCase(5, 18)]
         public void Can_Get_Calculate_Scores(int pins, int result)
         {
             FrameHits(pins, 20);
             Assert.That(result, Is.EqualTo(_game.Score()));
         }
+        #endregion
 
-        public void FrameHits(int pin, int count)
+        #region Private Methods
+        private void FrameHits(int pin, int count)
         {
             for (var i = 0; i < count; i++)
             {
@@ -30,11 +44,7 @@ namespace TDD_Katas_project.BowlingGame
 
             }
         }
+        #endregion
 
-        [TearDown]
-        public void TearDown()
-        {
-            _game = null;
-        }
     }
 }
