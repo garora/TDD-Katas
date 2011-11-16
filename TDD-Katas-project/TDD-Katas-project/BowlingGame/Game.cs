89ca25af1f6b31263df44aca31d5538db003d9cc
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-16 11:29:32 +0530

    Adding new stuff for The Bowling Game Kata

---
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.BowlingGame
+{
+    public class Game
+    {
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDD_Katas_project.BowlingGame
+{
+    [TestFixture]
+    public class TestGame
+    {
+    }
+}
