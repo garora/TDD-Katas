162ead9b09f991b4ef653f4ae32674b4f3ebeda2
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-16 17:09:35 +0530

    Rectified TestMethods

--- TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
@@ -6,18 +6,37 @@ namespace TDD_Katas_project.BowlingGame
     [TestFixture]
     public class TestGame
     {
+        private Game _game = null;
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
         [Test]
         [TestCase(0, 0)]
         public void Can_Get_Calculate_Scores(int pins, int result)
         {
-            var g = new Game();
+            
             for (var i = 0; i < 20; i++)
             {
-                g.Roll(pins);
+                _game.Roll(pins);
             }
-            Assert.That(result, Is.EqualTo(g.Score()));
+            Assert.That(result, Is.EqualTo(_game.Score()));
         }
-        
 
+        public void FrameHits(int pin)
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                _game.Roll(pin);
+
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _game = null;
+        }
     }
 }
