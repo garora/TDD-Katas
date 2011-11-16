299d1968aec865a96a6412d4319254fdf279019e
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-16 15:04:15 +0530

    Rectified Test

--- TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/TestGame.cs
@@ -7,18 +7,11 @@ namespace TDD_Katas_project.BowlingGame
     public class TestGame
     {
         [Test]
-        public void Can_Roll()
+        public void Can_Get_Calculate_Scores()
         {
             var g = new Game();
-            var result = g.Roll(0);
-            Assert.That(result, Is.EqualTo(0));
+            Assert.That(0, Is.EqualTo(g.Score()));
         }
-        [Test]
-        public void Can_Get_Score()
-        {
-            var g = new Game();
-            var result = g.Score();
-            Assert.That(result, Is.EqualTo(0));
-        }
+       
     }
 }
