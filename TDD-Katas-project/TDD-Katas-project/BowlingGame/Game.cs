96619ec5a89a6ad3edb4f7f565cca9439a6cba73
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-17 11:31:18 +0530

    Red Test continue

--- TDD-Katas-project/TDD-Katas-project/BowlingGame/Game.cs
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/Game.cs
@@ -16,18 +16,18 @@ namespace TDD_Katas_project.BowlingGame
         public int Score()
         {
             int score = 0;
-            int i = 0;
+            int frameIndex = 0;
             for (int frame = 0; frame < 10; frame++)
             {
-                if (rolls[i] + rolls[i + 1] == 10)
+                if (rolls[frameIndex] + rolls[frameIndex + 1] == 10)
                 {
-                    score += 10 + rolls[i + 2];
-                    i += 2;
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
                 }
                 else
                 {
-                    score += rolls[i] + rolls[i + 1];
-                    i += 2;
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
                 }
             }
             return _score;
