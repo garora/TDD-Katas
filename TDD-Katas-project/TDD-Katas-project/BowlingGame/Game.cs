881dc5648bae42575f6bb5e1d8dad0d0a0776896
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-18 00:53:12 +0530

    Fourth Test 'Green'

--- TDD-Katas-project/TDD-Katas-project/BowlingGame/Game.cs
+++ TDD-Katas-project/TDD-Katas-project/BowlingGame/Game.cs
@@ -19,11 +19,17 @@
             var frameIndex = 0;
             for (var frame = 0; frame < 10; frame++)
             {
-                if (IsSpare(frameIndex))
+                if (rolls[frameIndex] == 10)
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
                 {
                     score += 10 + rolls[frameIndex + 2];
                     frameIndex += 2;
                 }
+
                 else
                 {
                     score += rolls[frameIndex] + rolls[frameIndex + 1];
