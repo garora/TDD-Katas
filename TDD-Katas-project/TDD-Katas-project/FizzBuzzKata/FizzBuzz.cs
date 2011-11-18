c8aef3c61b03689f066beef6de898993128283de
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-19 01:45:32 +0530

    Refactoring

--- TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/FizzBuzz.cs
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/FizzBuzz.cs
@@ -6,23 +6,31 @@ using System.Text;
 
 namespace TDD_Katas_project.FizzBuzzKata
 {
-   public class FizzBuzz
+    public class FizzBuzz
     {
-       public static string PrintFizzBuzz()
-       {
-           var ResultFizzBuzz = string.Empty;
-           ResultFizzBuzz = GetNumbers(ResultFizzBuzz);
-           return ResultFizzBuzz;
-       }
+        public static string PrintFizzBuzz()
+        {
+            var ResultFizzBuzz = string.Empty;
+            ResultFizzBuzz = GetNumbers(ResultFizzBuzz);
+            return ResultFizzBuzz;
+        }
 
-       private static string GetNumbers(string ResultFizzBuzz)
-       {
-           for (int i = 1; i <= 100; i++)
-           {
-               if (i % 3 == 0) ResultFizzBuzz += " " + "Fizz";
-               ResultFizzBuzz += " " + (i).ToString();
-           }
-           return ResultFizzBuzz.Trim();
-       }
+        private static string GetNumbers(string ResultFizzBuzz)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var printNumber = string.Empty;
+                if (IsFizz(i)) printNumber += "Fizz";
+                if (String.IsNullOrEmpty(printNumber))
+                    printNumber = (i).ToString();
+                ResultFizzBuzz +=" "+ printNumber;
+            }
+            return ResultFizzBuzz.Trim();
+        }
+        
+        private static bool IsFizz(int i)
+        {
+            return i % 3 == 0;
+        }
     }
 }
