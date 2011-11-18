745044a0e28429af67769b920e01845da6eb135e
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-19 00:51:34 +0530

    Added new Kata FizzBuzz

---
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace TDD_Katas_project.FizzBuzzKata
+{
+   public class FizzBuzz
+    {
+       public static void PrintFizzBuzz()
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/TestFizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Katas_project.FizzBuzzKata
+{
+   [TestFixture]
+   [Category("TheFizzBuzzKata")]
+   public class TestFizzBuzz
+    {
+       [Test]
+       public void Can_Test_Fizz_Buzz()
+       {
+           FizzBuzz.PrintFizzBuzz();
+       }
+    }
+}
