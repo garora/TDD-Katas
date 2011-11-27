4e21830cd95ba6e8b93e99dc22b5ab7930216ee3
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-27 14:49:47 +0530

    Test1 'Red'

---
+++ TDD-Katas-project/TDD-Katas-project/OddEvenKata/OddEven.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.OddEvenKata
+{
+    public class OddEven
+    {
+        public static string PrintOddEven()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/OddEvenKata/TestOddEven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace TDD_Katas_project.OddEvenKata
+{
+    [TestFixture]
+    [Category("OddEven Kata")]
+    public class TestOddEven
+    {
+        [Test]
+        public void CanPrintOddEven()
+        {
+            var result = OddEven.PrintOddEven();
+            Assert.NotNull(result);
+        }
+    }
+}
