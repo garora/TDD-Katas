516e8efafee604cc971f6ef584f9b270b3fb33c1
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-27 17:46:59 +0530

    Test6 'Red'

--- TDD-Katas-project/TDD-Katas-project/OddEvenKata/TestOddEven.cs
+++ TDD-Katas-project/TDD-Katas-project/OddEvenKata/TestOddEven.cs
@@ -10,10 +10,23 @@ namespace TDD_Katas_project.OddEvenKata
     public class TestOddEven
     {
         [Test]
-        public void CanPrintOddEven()
+        [TestCase(1, 50)]
+        [TestCase(1, 100)]
+        [TestCase(5, 150)]
+        public void CanPrintOddEven(int startRange, int endRange)
         {
-            var result = OddEven.PrintOddEven(1,100);
-            Assert.NotNull(result,string.Format("{0}",result));
+            var result = OddEven.PrintOddEven(startRange, endRange);
+            Assert.NotNull(result, string.Format("{0}", result));
+        }
+        [Test]
+        [TestCase(1, "Odd")]
+        public void CanPrintOddEvenForSingleNumber(int number, string expectedresult)
+        {
+            var actualresult = OddEven.PringSingleOddEven(1);
+            Assert.That(expectedresult, Is.EqualTo(actualresult),
+                             string.Format("result of entered number [{0}] is [{1}] but it should be [{2}]", number,
+                                           actualresult, expectedresult));
+
         }
     }
 }
