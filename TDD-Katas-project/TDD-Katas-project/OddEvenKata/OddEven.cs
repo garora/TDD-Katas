ae8a9f698f4c2924b68c10854ee36dde5599d2e8
Gaurav Arora <gaurav.arora6@gmail.com>
2011-11-27 17:08:42 +0530

    Test3 'Green'

--- TDD-Katas-project/TDD-Katas-project/OddEvenKata/OddEven.cs
+++ TDD-Katas-project/TDD-Katas-project/OddEvenKata/OddEven.cs
@@ -15,12 +15,14 @@ namespace TDD_Katas_project.OddEvenKata
             for (var number = 1; number <= 100; number++)
             {
                 var newNumber = string.Empty;
+                
                 var oddNumber = IsOddNumber(number) ? "Odd" : Convert.ToString(number);
+                var primenumber = IsPrimeNumber(number) ? Convert.ToString(number) : oddNumber;
                 if (!string.IsNullOrEmpty(newNumber))
-                    newNumber += IsEvenNumber(number) ? "Even" : oddNumber;
+                    newNumber += IsEvenNumber(number) ? "Even" : primenumber;
                 else
                 {
-                    newNumber = IsEvenNumber(number) ? "Even" : oddNumber;
+                    newNumber = IsEvenNumber(number) ? "Even" : primenumber;
                 }
                 result += newNumber;
             }
@@ -28,7 +30,7 @@ namespace TDD_Katas_project.OddEvenKata
         }
         private static bool IsEvenNumber(int number)
         {
-            return number % 2 == 0;
+            return number > 2 && number % 2 == 0;
         }
         private static bool IsOddNumber(int number)
         {
@@ -36,7 +38,17 @@ namespace TDD_Katas_project.OddEvenKata
         }
         private static bool IsPrimeNumber(int number)
         {
-            return number%number == 0;
+            if (number < 2) { return false; }
+            if (number == 2) { return true; }
+            if (IsEvenNumber(number)) { return false; }
+            var divisble = 3;
+            while (divisble * divisble <= number)
+            {
+                if (number % divisble == 0) { return false; }
+                divisble += 2;
+            }
+            return true;
+
         }
     }
 }
