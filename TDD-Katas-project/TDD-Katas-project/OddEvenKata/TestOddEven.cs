47f18963e63296b2fa057dca533f69ebf20b65b6
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-06 00:32:54 +0530

    Merging changes

--- TDD-Katas-project/TDD-Katas-project/OddEvenKata/TestOddEven.cs
+++ TDD-Katas-project/TDD-Katas-project/OddEvenKata/TestOddEven.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+
 namespace TDD_Katas_project.OddEvenKata
 {
     [TestFixture]
@@ -11,9 +12,10 @@ namespace TDD_Katas_project.OddEvenKata
         [TestCase(5, 150)]
         public void CanPrintOddEven(int startRange, int endRange)
         {
-            var result = OddEven.PrintOddEven(startRange, endRange);
+            string result = OddEven.PrintOddEven(startRange, endRange);
             Assert.NotNull(result, string.Format("{0}", result));
         }
+
         [Test]
         [TestCase(1, "Odd")]
         [TestCase(3, "3")]
@@ -23,11 +25,10 @@ namespace TDD_Katas_project.OddEvenKata
         [TestCase(10, "Even")]
         public void CanPrintOddEvenForSingleNumber(int number, string expectedresult)
         {
-            var actualresult = OddEven.PrintSingleOddEven(number);
+            string actualresult = OddEven.PrintSingleOddEven(number);
             Assert.That(expectedresult, Is.EqualTo(actualresult),
-                             string.Format("result of entered number [{0}] is [{1}] but it should be [{2}]", number,
-                                           actualresult, expectedresult));
-
+                        string.Format("result of entered number [{0}] is [{1}] but it should be [{2}]", number,
+                                      actualresult, expectedresult));
         }
     }
 }
