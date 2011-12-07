ead7acec17c5b1de8111248b1500063b16b32374
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-08 01:12:11 +0530

    Test2 'Red'

--- TDD-Katas-project/TDD-Katas-project/The PrimeFactor Kata/PrimeFactorTest.cs
+++ TDD-Katas-project/TDD-Katas-project/The PrimeFactor Kata/PrimeFactorTest.cs
@@ -11,15 +11,23 @@ namespace TDD_Katas_project.The_PrimeFactor_Kata
     [Category("The PrimeFactor Kata")]
     public class PrimeFactorTest
     {
-        private static IList List()
+        private static IList<int> List(int ints)
         {
-            return new List<int>();
+            IList<int> list = new List<int>();
+            for (var i = 0; i < ints; i++)
+                list.Add(i);
+            return list;
         }
 
         [Test]
         public void Testone()
         {
-            Assert.That(List(),Is.EqualTo(PrimeFactor.Generate(1)));
+            Assert.That(List(0), Is.EqualTo(PrimeFactor.Generate(1)));
+        }
+        [Test]
+        public void Testwo()
+        {
+            Assert.That(List(2), Is.EqualTo(PrimeFactor.Generate(2)));
         }
     }
 }
