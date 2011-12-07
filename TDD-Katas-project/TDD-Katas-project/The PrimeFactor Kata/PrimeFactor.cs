8637d59e2f26c1bbce8f6ed9105527d8f46b2de6
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-08 01:51:57 +0530

    Test6 'Green'

--- TDD-Katas-project/TDD-Katas-project/The PrimeFactor Kata/PrimeFactor.cs
+++ TDD-Katas-project/TDD-Katas-project/The PrimeFactor Kata/PrimeFactor.cs
@@ -12,12 +12,12 @@ namespace TDD_Katas_project.The_PrimeFactor_Kata
             IList<int?> primes = new List<int?>();
             if (number > 1)
             {
-                if (number%2 == 0)
+                while (number % 2 == 0)
                 {
                     primes.Add(2);
                     number /= 2;
                 }
-                if(number>1)
+                if (number > 1)
                     primes.Add(number);
             }
             return primes;
