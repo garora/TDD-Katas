2181ade1fce30eb70c65ed14c2226d39b10f7faf
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-12 14:55:52 +0530

    Added few files for 'The LeapYear' Kata

---
+++ TDD-Katas-project/TDD-Katas-project/The LeapYear Kata/LeapYear.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.The_LeapYear_Kata
+{
+   public class LeapYear
+    {
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/The LeapYear Kata/LeapYearTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Katas_project.The_LeapYear_Kata
+{
+    [TestFixture]
+    public class LeapYearTest
+    {
+
+    }
+}
