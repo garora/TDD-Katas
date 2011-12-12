84cac329cb93d57d192760e238ec2a18432b7226
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-12 15:35:36 +0530

    Added more Test Cases

--- TDD-Katas-project/TDD-Katas-project/The LeapYear Kata/LeapYearTest.cs
+++ TDD-Katas-project/TDD-Katas-project/The LeapYear Kata/LeapYearTest.cs
@@ -3,12 +3,22 @@
 namespace TDD_Katas_project.The_LeapYear_Kata
 {
     [TestFixture]
+    [Category("The LeapYear")]
     public class LeapYearTest
     {
         [Test]
         public void CanTestForLeapYear()
         {
-            Assert.That(true,Is.EqualTo(LeapYear.IsLeapYear(1996)));
+            Assert.That(true, Is.EqualTo(LeapYear.IsLeapYear(1996)));
+        }
+
+        [Test]
+        [TestCase(false, 2001)]
+        [TestCase(true, 1996)]
+        [TestCase(true, 1992)]
+        public void CanTestForLeapYears(bool expectedResult, int year)
+        {
+            Assert.That(true, Is.EqualTo(LeapYear.IsLeapYear(1996)));
         }
     }
 }
