94720423f83076cdd84cb6c3ec08eefbc022d43d
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-27 18:23:59 +0530

    Test1 'Green'

--- TDD-Katas-project/TDD-Katas-project/The WordWrap Kata/WordWrap.cs
+++ TDD-Katas-project/TDD-Katas-project/The WordWrap Kata/WordWrap.cs
@@ -5,11 +5,23 @@ using System.Text;
 
 namespace TDD_Katas_project.The_WordWrap_Kata
 {
-   public class WordWrap
+    public class WordWrap
     {
-       public static string Wrap(string word, int wordLength)
-       {
-           throw new NotImplementedException();
-       }
+        public static string Wrap(string word, int wordLength)
+        {
+            var actualCount = 0;
+            var wrappedword = string.Empty;
+            foreach (var wrd in word)
+            {
+                wrappedword = wrappedword + Convert.ToString(wrd);
+                if (!char.IsWhiteSpace(wrd) || (wrd != '\n'))
+                {
+                    actualCount++;
+                    if (actualCount == wordLength)
+                        wrappedword += "\n";
+                }
+            }
+            return wrappedword;
+        }
     }
 }
