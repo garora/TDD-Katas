a1e0c8247f9bd68b44cfd48144bd2a762889eab7
Gaurav Arora <gaurav.arora6@gmail.com>
2011-12-27 18:00:25 +0530

    Added new class for 'Word Wrap Kata'

---
+++ TDD-Katas-project/TDD-Katas-project/The WordWrap Kata/WordWrap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.The_WordWrap_Kata
+{
+   public class WordWrap
+    {
+
+    }
+}
---
+++ TDD-Katas-project/TDD-Katas-project/The WordWrap Kata/WordWrapTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Katas_project.The_WordWrap_Kata
+{
+    [TestFixture]
+    [Category("The Word Wrap Kata")]
+    public class WordWrapTest
+    {
+
+    }
+}
