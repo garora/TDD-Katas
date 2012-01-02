e544abb14c10e7078f70a1f17f9d4b2387313a79
Gaurav Arora <gaurav.arora6@gmail.com>
2012-01-02 17:25:06 +0530

    Add List items Test - Red

--- TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedListTest.cs
+++ TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedListTest.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using NUnit.Framework;
 namespace TDD_Katas_project.The_RecentlyUsedList_kata
 {
     [TestFixture]
+    [Category("The Recently Used List Kata")]
     public class RecentlyUsedListTest
     {
+        #region Private Methods
+        private RecentlyUsedList<string> _recentlyUsedList;
+        private int _size;
+        #endregion
+        #region Setup/TearDown
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _recentlyUsedList = new RecentlyUsedList<string>();
+            _size = 10;
+        }
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            _recentlyUsedList = null;
+            _size = -1; //empty list
+        }
 
+
+        #endregion
+        [Test]
+        public void CanAddItems()
+        {
+            _recentlyUsedList.Add("FirstList");
+            var list = _recentlyUsedList.ToList();
+            Assert.NotNull(list, string.Format("List should contain {0} but it is {1}", "FirstList", null));
+        }
     }
 }
