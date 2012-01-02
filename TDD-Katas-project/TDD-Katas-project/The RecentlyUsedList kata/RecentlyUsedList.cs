d00782f97f3bedc4cce7a0c783326b5b9bf56ded
Gaurav Arora <gaurav.arora6@gmail.com>
2012-01-02 15:09:20 +0530

    Added description of Kata - what to do in this kata?

--- TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedList.cs
+++ TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedList.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿#region Description of Kara - Recently Used List
+//Recently Used List :
+
+//    Develop a recently-used-list class to hold strings 
+//    uniquely in Last-In-First-Out order.
+
+//    o) The most recently added item is first, the least
+//       recently added item is last.
+
+//    o) Items can be looked up by index, which counts from zero.
+
+//    o) Items in the list are unique, so duplicate insertions
+//       are moved rather than added.
+
+//    o) A recently-used-list is initially empty.
+
+//    Optional extras
+
+//    o) Null insertions (empty strings) are not allowed.
+
+//    o) A bounded capacity can be specified, so there is an upper
+//       limit to the number of items contained, with the least
+//       recently added items dropped on overflow.
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace TDD_Katas_project.The_RecentlyUsedList_kata
 {
-    class RecentlyUsedList
+   public class RecentlyUsedList
     {
+
     }
 }
--- TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedListTest.cs
+++ TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedListTest.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using NUnit.Framework;
 namespace TDD_Katas_project.The_RecentlyUsedList_kata
 {
-    class RecentlyUsedListTest
+    [TestFixture]
+    public class RecentlyUsedListTest
     {
+
     }
 }
