6dc9f0c908ea11fe0b8840bd6f1f4b703851feac
Gaurav Arora <gaurav.arora6@gmail.com>
2012-01-02 14:56:35 +0530

    Added skeleton for 'The Recently used List Kata'

---
+++ TDD-Katas-project/TDD-Katas-project/The RecentlyUsedList kata/RecentlyUsedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.The_RecentlyUsedList_kata
+{
+    class RecentlyUsedList
+    {
+    }
+}
