c258e33ce6843ecba6755ec4738a07bb4d78ca5a
Gaurav Arora <g_arora@hotmail.com>
2013-08-18 19:23:29 +0530

    removed commented code

--- TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/TestFizzBuzz.cs
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/TestFizzBuzz.cs
@@ -16,7 +16,6 @@ namespace TDD_Katas_project.FizzBuzzKata
         [TestFixtureSetUp]
         public void Setup()
         {
-            //_result = @"1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100";
             _resultFizz = @"1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz";
         }
         [TestFixtureTearDown]
@@ -29,13 +28,6 @@ namespace TDD_Katas_project.FizzBuzzKata
 
         #region Test Methods
 
-        //[Test]
-        //[Ignore]
-        //public void CanTestFizzBuzz()
-        //{
-        //    Console.WriteLine(FizzBuzz.PrintFizzBuzz());
-        //    Assert.That(_result, Is.EqualTo(FizzBuzz.PrintFizzBuzz()));
-        //}
         [Test]
         public void CanTestFizz()
         {
