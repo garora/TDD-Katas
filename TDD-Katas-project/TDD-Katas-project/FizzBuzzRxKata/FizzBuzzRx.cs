d4db8f4a6db44a245a192fd01490042ba3c6a958
dhelper <dror.helper@gmail.com>
2015-03-03 09:19:27 +0200

    Added FizzBuzz using Rx

---
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzRxKata/FizzBuzzRx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace TDD_Katas_project.FizzBuzzRxKata
+{
+    public class FizzBuzzRx
+    {
+        public static string Generate(int max)
+        {
+            var result = string.Empty;
+            if (max <= 0)
+            {
+                return result;
+            }
+
+            var observable = Observable.Range(1, max);
+
+            var dividedByThree = observable
+                .Where(i => i % 3 == 0)
+                .Select(_ => "Fizz");
+
+            var dividedByFive = observable
+                .Where(i => i % 5 == 0)
+                .Select(_ => "Buzz");
+
+            var simpleNumbers = observable
+               .Where(i => i % 3 != 0 && i % 5 != 0)
+               .Select(i => i.ToString());
+
+            var commaDelimiter = observable.Select(_ => ",");
+
+            IObservable<string> specialCases = (dividedByThree).Merge(dividedByFive);
+            simpleNumbers
+                .Merge(specialCases)
+                .Merge(commaDelimiter)
+                .Subscribe(s => result += s);
+
+            return result;
+        }
+    }
+}
