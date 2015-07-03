f06d197e17c9c8d194d5a20ee5cf4285842a49ec
Gaurav Arora <g_arora@hotmail.com>
2015-07-04 04:09:07 +0530

    refactoring

--- TDD-Katas-project/TDD-Katas-project/StringSum/StringSumKata.cs
+++ TDD-Katas-project/TDD-Katas-project/StringSum/StringSumKata.cs
@@ -10,12 +10,21 @@ namespace TDD_Katas_project.StringSum
     {
         public static string Sum(string num1, string num2)
         {
-            var realNum1 = string.IsNullOrEmpty(num1) ? "0" : num1;
-            var realNum2 = string.IsNullOrEmpty(num2) ? "0" : num2;
+            var realNum1 = GetZeroWhenNullOrEmpty(num1);
+            var realNum2 = GetZeroWhenNullOrEmpty(num2);
 
-            return Convert.ToString(int.Parse(realNum1) + int.Parse(realNum2));
+            return Convert.ToString(Add(realNum1, realNum2));
         }
 
+        private static string GetZeroWhenNullOrEmpty(string num1)
+        {
+            return string.IsNullOrEmpty(num1) ? "0" : num1;
+        }
+
+        private static int Add(string realNum1, string realNum2)
+        {
+            return int.Parse(realNum1) + int.Parse(realNum2);
+        }
 
     }
 }
