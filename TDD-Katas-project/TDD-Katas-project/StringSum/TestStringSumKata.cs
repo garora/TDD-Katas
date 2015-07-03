2611d2daf1caad8de787adadccbee821705098a5
Gaurav Arora <g_arora@hotmail.com>
2015-07-04 03:52:17 +0530

    initial test

--- TDD-Katas-project/TDD-Katas-project/StringSum/TestStringSumKata.cs
+++ TDD-Katas-project/TDD-Katas-project/StringSum/TestStringSumKata.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TDD_Katas_project.StringSum
 {
-    class TestStringSumKata
+    [TestFixture]
+    [Category("StringSumkata")]
+    public class TestStringSumKata
     {
+        [TestCase("",null,0)]
+        [Test]
+        public void AddReturnSum(string num1, string num2, string expectedResult)
+        {
+            //call method here
+        }
     }
 }
