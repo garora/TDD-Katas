417602b919264f213f7e98ce6e0001c2d6da6b2a
Gaurav Arora <g_arora@hotmail.com>
2018-03-27 22:01:27 +0000

    merging and fixing issues: There is a better way to implment Password Kata adding as it came with PR need to refactor

---
+++ Src/CSharp/Net Framework/PasswordVerifier/PasswordVerifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace TDD_Katas_project.PasswordVerifier
+{
+    public class PasswordVerifier
+    {
+        private int _counter;
+
+        public string Verify(string password)
+        {
+            password = password.Trim();
+            var message = "Password is beingChecked";
+            if (password.Length > 8)
+                _counter += 1;
+            else
+                throw new ArgumentException("password is too short");
+            if (!string.IsNullOrEmpty(password))
+                _counter += 1;
+            else
+                throw new ArgumentException("password is null");
+            if (password.Any(char.IsUpper))
+                _counter += 1;
+            else
+                throw new ArgumentException("password doesn't contain a uppercase element");
+            if (password.Any(char.IsLower))
+                _counter += 1;
+            else
+                throw new ArgumentException("password doesn't contain a lowercase element");
+            if (password.Any(char.IsDigit))
+                _counter += 1;
+            else
+                throw new ArgumentException("password doesn't contain a number");
+            if (_counter >= 3 && password.Length > 8 && password.Any(char.IsLower))
+                message = message.Replace("beingChecked", "OK");
+            else
+                message = message.Replace("beingChecked", "never Ok");
+            return message;
+        }
+    }
+}
---
+++ Src/CSharp/Net Framework/PasswordVerifier/TestPasswordVerifier.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace TDD_Katas_project.PasswordVerifier
+{
+    [TestFixture]
+    [Category("TestPasswordVerifierKata")]
+    public class TestPasswordVerifier
+    {
+        #region Setup/TearDown
+
+        #endregion
+
+        #region TestMethods
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("1234567")]
+        [Test]
+        public void ShouldThrowExceptionIfPasswordDoesNotMeetTheRules(string password)
+        {
+            
+        }
+        
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
