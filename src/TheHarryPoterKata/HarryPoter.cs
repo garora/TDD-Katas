9041ee070baea8799cc646c9b946798461d3c1dd
Gaurav Arora <g_arora@hotmail.com>
2021-03-14 22:33:56 +0530

    TODO: HarryPoter

--- src/TheHarryPoterKata/HarryPoter.cs
+++ src/TheHarryPoterKata/HarryPoter.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 namespace TDD_Katas_project.TheHarryPoterKata
 {
+    /// <summary>
+    /// TODO: HarryPoter
+    /// </summary>
     public class HarryPoter
     {
     }
