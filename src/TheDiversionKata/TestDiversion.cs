579ceabf72f4993dd8b572dca91b95638002c68d
Gaurav Arora <g_arora@hotmail.com>
2021-03-14 22:32:31 +0530

    TODO: Diversion

--- src/TheDiversionKata/TestDiversion.cs
+++ src/TheDiversionKata/TestDiversion.cs
@@ -4,6 +4,9 @@ using NUnit.Framework;
 
 namespace TDD_Katas_project.TheDiversionKata
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [TestFixture]
     [Category("DiversionKata")]
     public class TestDiversion
